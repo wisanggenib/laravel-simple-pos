4cb541b7416d0c4fdb2d6ee4dfe50c0462755a36
Ubuntu <wisanggenib@testing-laravel.neo.internal>
2024-06-03 13:22:32 +0700

    update

--- public/index.php
+++ public/index.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
@@ -15,3 +14,4 @@ require __DIR__.'/../vendor/autoload.php';
 // Bootstrap Laravel and handle the request...
 (require_once __DIR__.'/../bootstrap/app.php')
     ->handleRequest(Request::capture());
+?>
